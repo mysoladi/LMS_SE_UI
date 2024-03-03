7e0dd919993fa380f9be4cbc949eb9bb889a0463
Kolusu, Vandana <vakolusu@iu.edu>
2024-03-03 15:01:53 -0500

    adding user dashboard page

---
+++ src/pages/assets/Home.jsx
@@ -0,0 +1,41 @@
+import {
+    Box,
+    Grid,
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    Typography
+} from "@mui/material";
+import CalendarStud from "./CalendarStud";
+import AssignmentsTable from "./AssignmentsTable";
+import AnnouncementsTable from "./AnnouncementsTable";
+import {text} from "@fortawesome/fontawesome-svg-core";
+// import CoursesPage from "./CoursesPage";
+
+
+export default function DashboardHome() {
+
+    return (
+        <Box sx={{marginLeft:2}}>
+            {/*<Grid container spacing={2} >*/}
+            {/*    <Grid container spacing={2} sx={{ maxWidth: 2000, mt: 0 }}>*/}
+            {/*        <CoursesPage />*/}
+            {/*    </Grid>*/}
+            {/*    <Grid container marginLeft={2}>*/}
+                    <div style={{textAlign:"left"}}>
+                        <span className='heading'>Welcome to EduNexa</span>
+                    </div>
+                    <Grid item xs={12} md={6}>
+                        <AssignmentsTable />
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{marginRight:0}} marginRight={0}  >
+                        <AnnouncementsTable />
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{marginLeft:2}}>
+                        <CalendarStud />
+                    </Grid>
+
+        </Box>
+    );
+}
